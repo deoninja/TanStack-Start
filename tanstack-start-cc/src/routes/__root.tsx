cabd156ac4b6abd83b86274758b800045a26ab45
Deo <deotri4@gmail.com>
2026-04-21 22:31:43 +0800

    route

--- tanstack-start-cc/src/routes/__root.tsx
+++ tanstack-start-cc/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router';
-// import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-// import { TanStackDevtools } from '@tanstack/react-devtools';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
 
 import appCss from '../styles.css?url';
 
@@ -40,7 +40,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body className='font-sans antialiased [overflow-wrap:anywhere] selection:bg-[rgba(79,184,178,0.24)]'>
         {children}
 
-        {/* <TanStackDevtools
+        <TanStackDevtools
           config={{
             position: 'bottom-right',
           }}
@@ -50,7 +50,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
               render: <TanStackRouterDevtoolsPanel />,
             },
           ]}
-        /> */}
+        />
         <Scripts />
       </body>
     </html>
--- tanstack-start-cc/src/routes/index.tsx
+++ tanstack-start-cc/src/routes/index.tsx
@@ -1,14 +1,39 @@
 import SkillCard from '#/components/SkillCard';
 import { createFileRoute } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/')({ component: App });
+const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export const Route = createFileRoute('/')({ component: App,
+
+  pendingComponent: () => (<div className='p-14 text-center text-shadow-amber-50'>Loading Pokemon...</div>),
+  pendingMs: 300,
+
+  loader: async () => {
+    // const skills = await fetch('/api/skills').then(res => res.json());
+    // return { skills };
+    // console.log('Loading data for "/" route...');
+    const response = await fetch(POKE_API_URL);
+    const data = await response.json();
+    console.log('Loading data for "/" route...', data);
+    return data;
+  }
+ });
 
 function App() {
+  const data = Route.useLoaderData();
   return (
     <main className='page-wrap px-4 pb-8 pt-14'>
       <h1 className='text-3xl font-bold'>Hello, world!</h1>
 
-      <ul>
+      <ul className='mt-6 list-none p-0 space-y-5'>
+        {data.results.map((pokemon: { name: string }) => (
+          <li key={pokemon.name}>
+            <SkillCard name={pokemon.name} />
+          </li>
+        ))} 
+      </ul>
+
+      {/* <ul className='mt-6 list-none p-0 space-y-5'>
         <li>
           <SkillCard name='TanStack Start' />
         </li>
@@ -18,7 +43,7 @@ function App() {
         <li>
           <SkillCard name='MongoDB' />
         </li>
-      </ul>
+      </ul> */}
     </main>
   );
 }
